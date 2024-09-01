4cfe157abefc2fe913ffb590ff78f9cb3e072890
Parth Paradkar <parthparadkar3@gmail.com>
2024-09-01 12:47:05 +0530

    Fix build

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -5,11 +5,6 @@ import { ItemEditLine } from "./ItemLine";
 import { Button } from "./ui/button";
 import ImageUploader from "./ImageUploader";
 
-type ApiItem = {
-  QUANTITY: number;
-  ITEM: string;
-  PRICE: number;
-}
 
 export const ItemList = () => {
   const { items, addItem, setItems } = useContext(ItemsContext);
@@ -19,28 +14,9 @@ export const ItemList = () => {
     console.log("Inside addEmptyItem", items);
   };
 
-  const processImage = async (imageStr: string) => {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    const headers = {
-      "Content-Type": "application/json",
-    }
-    if (!apiUrl) return;
-    const response = await axios.post<ApiItem[]>(apiUrl, { image: imageStr }, {
-      headers
-    });
-    const itemsList = response.data.map((item) => {
-      return {
-        name: item.ITEM,
-        price: item.PRICE,
-        users: []
-      }
-    })
-    setItems(itemsList)
-  }
-
   return (
     <div className="my-8">
-      <ImageUploader onUpload={processImage}/>
+      <ImageUploader />
       {items.map((item, index) => (
         <ItemEditLine key={index} index={index} />
       ))}
